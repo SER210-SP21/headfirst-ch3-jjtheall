3a4af37755c86cc41e40171a68e039c08c1b810e
jjtheall <jtheall@quinnipiac.edu>
2022-02-06 19:33:42 -0500

    initial commit, entire demo built

---
+++ app/src/main/java/edu/quinnipiac/ser210/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,36 @@
+/*
+2/6/22
+@author: jackjtheall
+ */
+
+package edu.quinnipiac.ser210.mymessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //call onSendMessage when button is clicked
+    public void onSendMessage(View view){
+        EditText messageView = (EditText)findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        //Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        //intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+        //startActivity(intent);
+        startActivity(chosenIntent);
+    }
+}
---
+++ app/src/main/java/edu/quinnipiac/ser210/mymessenger/ReceiveMessageActivity.java
@@ -0,0 +1,26 @@
+/*
+2/6/22
+@author: jackjtheall
+ */
+
+package edu.quinnipiac.ser210.mymessenger;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.TextView;
+
+public class ReceiveMessageActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = (TextView)findViewById(R.id.message);
+        messageView.setText(messageText);
+    }
+}
